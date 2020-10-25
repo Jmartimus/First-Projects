10c44427ecd8685ecb44088f16218f9bbc2f7d74
Jmartimus <66697301+Jmartimus@users.noreply.github.com>
2020-10-25 13:54:21 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
-import React, {useState} from "react";
+import React from "react";
 import "./styles.css";
 
+const todos = [
+  {
+    isComplete: false,
+    task: "Mow the yard"
+  },
+  {
+    isComplete: true,
+    task: "Clean the kitchen"
+  },
+  {
+    isComplete: false,
+    task: "Cook dinner"
+  }
+];
+const todoList = todos.map((todo) => {
+  const isComplete = todo.isComplete;
+  return(
+    <li key={todo.task} 
+        style={{textDecoration: isComplete ? 'line-through' : undefined}}>
+      {todo.task}</li> 
+    );
+  }
+)
+
+
 export default function App() {
-  
-  const [count, setCount] = useState(0);
-  
   return (
     <div className="App">
-      <h1>I&apos;ve been clicked {count} times.</h1>
-
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>My Todos</h1>
+      <ul>
+        {todoList}
+        </ul>
     </div>
   );
 }
-
